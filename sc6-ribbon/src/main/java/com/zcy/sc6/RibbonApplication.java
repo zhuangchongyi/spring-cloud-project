2c0663bbd10a25f80704066c471631651559b4e0
zhuangchongyi <1909017815@qq.com>
2020-07-23 17:09:01 +0800

    ribbon实现负载均衡

--- sc6-ribbon/src/main/java/com/zcy/sc6/RibbonApplication.java
+++ sc6-ribbon/src/main/java/com/zcy/sc6/RibbonApplication.java
@@ -3,6 +3,7 @@ package com.zcy.sc6;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,7 +17,9 @@ public class RibbonApplication {
 
     /**
      * 创建 RestTemplate 实例，并存入 spring 容器
+     * @LoadBalanced 负载均衡注解，会对 RestTemplate 实例进行封装，创建动态代理对象，并切入（AOP）负载均衡代码，把请求分散分发到集群中的服务器
      */
+    @LoadBalanced //负载均衡注解
     @Bean
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
